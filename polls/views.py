cfca8b2290352226988db17a551c781cb313732b
git-palace <edeveloper4@gmail.com>
2018-11-08 14:40:51 -0800

    change url of question object in index template

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 # Create your views here.
@@ -11,26 +10,10 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    response = "You're looking at question %s"
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
