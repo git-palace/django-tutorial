ce5f84bd2346b35051af8faaf5a884ea610f859b
git-palace <edeveloper4@gmail.com>
2018-11-08 13:41:21 -0800

    work on views and templates

--- polls/views.py
+++ polls/views.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
