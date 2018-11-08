427ec7e4712126b315806731d17db1a6e01ec904
git-palace <edeveloper4@gmail.com>
2018-11-08 13:37:28 -0800

    updates

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ import views
 
 urlpatterns = [
     url('', views.index, name='index'),
+    url('<int:question_id>/', views.detail, name='detail'),
+    url('<int:question_id>/results', views.results, name='results'),
+    url('<int:question_id>/vote', views.vote, name='vote'),
 ]
